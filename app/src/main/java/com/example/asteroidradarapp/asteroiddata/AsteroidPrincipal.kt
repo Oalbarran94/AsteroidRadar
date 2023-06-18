e85109da2aeed338cb2f762656141e5dde96719b
Osman <oalbarran.1994@gmail.com>
2023-06-18 18:45:20 -0400

    Adding menu

--- app/src/main/java/com/example/asteroidradarapp/asteroiddata/AsteroidPrincipal.kt
+++ app/src/main/java/com/example/asteroidradarapp/asteroiddata/AsteroidPrincipal.kt
@@ -1,13 +1,17 @@
 package com.example.asteroidradarapp.asteroiddata
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.example.asteroidradarapp.R
 import com.example.asteroidradarapp.databinding.FragmentAsteroidPrincipalBinding
 
 
@@ -37,6 +41,22 @@ class AsteroidPrincipal : Fragment() {
             }
         }
 
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.asteroid_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.view_today_asteroids -> Log.i("view today asteroids","view today asteroids")
+            R.id.view_saved_asteroids -> Log.i("view saved asteroids","view saved asteroids")
+            R.id.view_week_asteroids -> Log.i("view week asteroids","view week asteroids")
+        }
+        return true
     }
 }
